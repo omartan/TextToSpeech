f7c1627e3853b42f8aa3b7c7869492c8450b17b8
Omar Tan <omartan90@hotmail.com>
2020-06-09 21:45:26 +0800

    Initial Commit

---
+++ TextToSpeech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextToSpeech
+//
+//  Created by Omar Tan Johan Tan on 09/06/20.
+//  Copyright © 2020 Omar Tan Johan Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
