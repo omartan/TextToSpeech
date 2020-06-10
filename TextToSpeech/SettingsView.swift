53480509507b0bd7794518b17a776f5d1e7c2fde
Omar Tan <omartan90@hotmail.com>
2020-06-11 00:10:48 +0800

    Settings view

---
+++ TextToSpeech/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  TextToSpeech
+//
+//  Created by Omar Tan Johan Tan on 10/06/20.
+//  Copyright © 2020 Omar Tan Johan Tan. All rights reserved.
+//
+
+import UIKit
+
+class SettingsView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func done(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
