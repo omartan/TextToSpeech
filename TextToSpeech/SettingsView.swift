4d0d287ae98b210ceb9a8269bc830ea3f9e07a26
Omar Tan <omartan90@hotmail.com>
2020-06-11 15:11:28 +0800

    PickerView added content

--- TextToSpeech/SettingsView.swift
+++ TextToSpeech/SettingsView.swift
@@ -6,12 +6,34 @@
 //  Copyright © 2020 Omar Tan Johan Tan. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
-class SettingsView: UIViewController {
+class SettingsView: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet var pickerView: UIPickerView!
+    
+    var voicesArray: [String] = []
+    let speechVoices = AVSpeechSynthesisVoice.speechVoices()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        pickerView.delegate = self
+        pickerView.dataSource = self
     }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return speechVoices.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return speechVoices[row].language
+    }
+    
     @IBAction func done(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
